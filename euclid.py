7a6e211fbcc7366649f81232939c259a279846f3
Enrico R <enricorrx@gmail.com>
2021-11-20 19:30:52 +0100

    Refactor

--- euclid.py
+++ euclid.py
@@ -1,19 +1,18 @@
-def euclid(a,b):
-	if b == 0:
-		return a
-	return euclid(b, a % b)
+def euclid(c, d):
+	if d == 0:
+		return c
+	return euclid(d, c % d)
+
 
 # iterative Euclid
-def it_euclid(a,b):
-	while(b != 0):
-		a, b = b, a % b
-	return a
+def it_euclid(c, d):
+	while d != 0:
+		c, d = d, c % d
+	return c
 		
 
 if __name__ == "__main__":
-	while(True):
-		print("Calcolo il Greatest Common Divider (gcd)")
-		a = int(input("Inserisci a: "))
-		b = int(input("Inserisci b: "))
-		print("gcd("+str(a)+","+str(b)+") =", abs(it_euclid(a,b))) # gcd is always positive!
-		print("Premi CTRL+C per uscire...\n")
+	print("Compute the Greatest Common Divider (gcd)")
+	a = int(input("Insert a: "))
+	b = int(input("Insert b: "))
+	print(f"gcd({a},{b}) = {abs(it_euclid(a,b))}")  # gcd is always positive!
